00ef9cfbf6861658e528b1fc1e7e3c3810525289
Cao yutian <804860914@qq.com>
2026-03-14 09:29:24 +0800

    Add files via upload

---
+++ Submission_1/_Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int age = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
